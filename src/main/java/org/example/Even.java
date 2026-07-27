6c101f8fd5ded6a432db8ea591cf6c5eb0fd7e7e
Kim Dariya <dkim5354@gmail.com>
2026-07-27 20:09:13 +0700

    initial commit

---
+++ src/main/java/org/example/Even.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Even {
+
+    public static void even(Scanner scanner) {
+
+        Greet.greeting(scanner);
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        Random random = new Random();
+        int correctCounter = 0;
+
+        while (true) {
+
+            int num = random.nextInt(100) + 1;
+            System.out.println("Question: " + num);
+
+            System.out.print("Your answer: ");
+            String ans = scanner.nextLine().toLowerCase();
+
+            if (!ans.equals("yes") && !ans.equals("no")) {
+                System.out.println("Let's try again, " + Greet.name + "!");
+                correctCounter = 0;
+                continue;
+            }
+
+            if (num % 2 == 0 && ans.equals("yes")) {
+                System.out.println("Correct!");
+                correctCounter++;
+            } else if (num % 2 != 0 && ans.equals("no")) {
+                System.out.println("Correct!");
+                correctCounter++;
+            } else {
+                System.out.println("Let's try again, " + Greet.name + "!");
+                correctCounter = 0;
+            }
+
+            if (correctCounter == 3) {
+                System.out.println("Congratulations, " + Greet.name + "!");
+                break;
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/Prime.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prime {
+    public static void prime(Scanner scanner) {
+        Greet.greeting(scanner);
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        int correctCounter = 0;
+        Random random = new Random();
+
+        while (true) {
+            int num = random.nextInt(100) + 1;
+
+            System.out.println("Question: " + num);
+            System.out.print("Your answer: ");
+            String ans = scanner.nextLine().toLowerCase();
+
+            if ((isPrime(num) && ans.equals("yes")) || (!isPrime(num) && ans.equals("no"))) {
+                correctCounter++;
+                System.out.println("Correct!");
+            } else {
+                String correctAnswer = isPrime(num) ? "yes" : "no";
+                System.out.println("'" + ans + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                System.out.println("Let's try again, " + Greet.name + "!");
+                correctCounter = 0;
+            }
+
+            if (correctCounter == 3) {
+                System.out.println("Congratulations, " + Greet.name + "!");
+                break;
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        int[] arr = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        boolean flag = false;
+        for (int i : arr) {
+            if (num == i) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+
+}
